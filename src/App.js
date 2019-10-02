7e818577fad44376cfb77a6dc01030abb571bb12
Shpakova Alena <shpakovadev@gmail.com>
2019-10-02 15:28:21 +0300

    html and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Button from '@material-ui/core/Button'
 
-function App() {
+const App = () => {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="wrapper">
+      <Paper className="wrapper-menu">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered
         >
-          Learn React
-        </a>
-      </header>
+          <Tab label="How it works" />
+          <Tab label="Why we" />
+          <Tab label="About us" />
+          <Tab label="Contacts" />
+        </Tabs>
+      </Paper>
+      <div className="wrapper-button">
+        <Button>Sign in</Button>
+        <Button variant="contained" color="secondary">
+          Sign up
+        </Button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
